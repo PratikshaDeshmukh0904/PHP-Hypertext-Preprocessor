78832ae952a26d0bf3b4d6b664352b2bfadd22f0
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-09-25 15:11:25 +0530

    php array to newer square bracket

---
+++ PHP Programs/newphp.php
@@ -0,0 +1,38 @@
+
+
+<?php
+// $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+// echo json_encode([$age]);
+
+
+$date = array();
+$date[] = array('01-09-22',65);
+$date[] = array('02-09-22',4);
+$date[] = array('03-09-22',52);
+$date[] = array('04-09-22',54);
+$date[] = array('05-09-22',59);
+$date[] = array('06-09-22',51);
+$date[] = array('07-09-22',59);
+$date[] = array('08-09-22',89);
+$date[] = array('09-09-22',79);
+$date[] = array('10-09-22',59);
+$date[] = array('11-09-22',31);
+$date[] = array('12-09-22',23);
+$date[] = array('13-09-22',59);
+$date[] = array('14-09-22',59);
+$date[] = array('15-09-22',89);
+$date[] = array('16-09-22',52);
+$date[] = array('17-09-22',45);
+$date[] = array('18-09-22',42);
+$date[] = array('19-09-22',46);
+$date[] = array('20-09-22',47);
+$date[] = array('21-09-22',46);
+$date[] = array('22-09-22',42);
+$date[] = array('23-09-22',42);
+$date[] = array('24-09-22',46);
+$date[] = array('25-09-22',48);
+echo json_encode($date);
+
+
+?>
